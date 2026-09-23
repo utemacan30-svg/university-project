b47e8cf7f4d624beee3c88b960dc66200eed98b1
unknown <utemacan30@gmail.com>
2026-09-23 13:00:08 +0300

    Initial project

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json({ limit: "50mb" }));
+
+app.post("/upload", (req, res) => {
+    const imageData = req.body.image;
+
+    if (!imageData) {
+        return res.status(400).send("No image data");
+    }
+
+    const base64Data = imageData.replace(/^data:image\/png;base64,/, "");
+
+    const filename = `image_${Date.now()}.png`;
+    const filepath = path.join(__dirname, "images", filename);
+
+    fs.writeFile(filepath, base64Data, "base64", (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Error saving image");
+        }
+
+        console.log(`Image saved: ${filename}`);
+        res.send("Image uploaded successfully");
+    });
+});
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
